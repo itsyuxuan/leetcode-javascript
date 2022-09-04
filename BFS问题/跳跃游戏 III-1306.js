408b7ebfad49395c791647c468ff37031dd66267
Yuxuan Lin <yuxuan.l@icloud.com>
2022-09-04 12:11:56 +0800

    1306

--- BFS问题/跳跃游戏 III-1306.js
+++ BFS问题/跳跃游戏 III-1306.js
@@ -3,25 +3,22 @@
  * @param {number} start
  * @return {boolean}
  */
-let canReach = function (arr, start) {
-  let n = arr.length
-  let visited = []
-  let queue = [start]
+const canReach = function (arr, start) {
+  const n = arr.length
+  const visited = []
+  const queue = [start]
   while (queue.length) {
-      let index = queue.pop()
-      let val = arr[index]
-      if (val === 0) {
-          return true
-      }
-      let left = index - val
-      let right = index + val
-      if (left >= 0 && !visited[left]) {
-          queue.push(left)
-      }
-      if (right < n && !visited[right]) {
-          queue.push(right)
-      }
-      visited[index] = true
+    const i = queue.pop()
+    const val = arr[i]
+    if (val === 0)
+      return true
+    const left = i - val
+    const right = i + val
+    if (left >= 0 && !visited[left])
+      queue.push(left)
+    if (right < n && !visited[right])
+      queue.push(right)
+    visited[i] = true
   }
   return false
-};
+}
--- BFS问题/跳跃游戏 IV-1345.js
+++ BFS问题/跳跃游戏 IV-1345.js
@@ -2,24 +2,23 @@
  * @param {number[]} arr
  * @return {number}
  */
-let minJumps = function (arr) {
+const minJumps = function (arr) {
   let n = arr.length
-  if (n === 1) {
+  if (n === 1)
     return 0
-  }
 
   // 连续出现超过两次的数字就抛弃掉
-  let newArr = []
+  const newArr = []
   let sameCount = 0
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === arr[i - 1]) {
       sameCount += 1
-      if (sameCount >= 2) {
+      if (sameCount >= 2)
         continue
-      } else {
+      else
         newArr.push(arr[i])
-      }
-    } else {
+    }
+    else {
       newArr.push(arr[i])
       sameCount = 0
     }
@@ -27,42 +26,41 @@ let minJumps = function (arr) {
   arr = newArr
   n = arr.length
   // 遍历一遍 记录每个数字出现的下标位置
-  let indexesMap = new Map()
+  const indexesMap = new Map()
   for (let i = 0; i < n; i++) {
-    let val = arr[i]
-    let indexes = indexesMap.get(val)
-    if (!indexes) {
+    const val = arr[i]
+    const indexes = indexesMap.get(val)
+    if (!indexes)
       indexesMap.set(val, [i])
-    } else {
+    else
       indexes.push(i)
-    }
   }
 
-  let visited = []
+  const visited = []
   let count = 0
-  let queue = [0]
+  const queue = [0]
   while (queue.length) {
     count++
-    let len = queue.length
+    const len = queue.length
     for (let i = 0; i < len; i++) {
-      let index = queue.shift()
+      const index = queue.shift()
       // 找到了 由于bfs的特性 此时用的跳跃次数一定是最少的
-      if (index === n - 1) {
+      if (index === n - 1)
         return count - 1
-      }
 
       // 没找到 继续把可以跳的几个位置都放入队列中
-      let val = arr[index]
-      let left = index - 1
-      let right = index + 1
-      let sameIndexes = indexesMap.get(val)
+      const val = arr[index]
+      const left = index - 1
+      const right = index + 1
+      const sameIndexes = indexesMap.get(val)
 
-      if (left >= 0 && !visited[left]) queue.push(left)
-      if (right < n && !visited[right]) queue.push(right)
-      for (let sameIndex of sameIndexes) {
-        if (sameIndex !== index && !visited[sameIndex]) {
+      if (left >= 0 && !visited[left])
+        queue.push(left)
+      if (right < n && !visited[right])
+        queue.push(right)
+      for (const sameIndex of sameIndexes) {
+        if (sameIndex !== index && !visited[sameIndex])
           queue.push(sameIndex)
-        }
       }
 
       visited[index] = true
