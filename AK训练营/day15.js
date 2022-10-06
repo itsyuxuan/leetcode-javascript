b13867ebbab4edde6cfc9c6ba7905f552c13510a
Yuxuan Lin <yuxuan.l@icloud.com>
2022-10-06 18:22:04 +0800

    day15

--- AK训练营/day15.js
+++ AK训练营/day15.js
@@ -41,3 +41,33 @@ const levelOrder = function (root) {
   }
   return res
 }
+
+// LC.226 https://leetcode-cn.com/problems/invert-binary-tree/
+// 翻转二叉树
+// 前序遍历
+const invertTree = function (root) {
+  if (root) {
+    [root.left, root.right] = [root.right, root.left]
+    root.left = invertTree(root.left)
+    root.right = invertTree(root.right)
+  }
+  return root
+}
+
+// LC.101 https://leetcode-cn.com/problems/symmetric-tree/
+// 对称二叉树
+// 递归法
+const isSymmetric = function (root) {
+  const compare = (l, r) => {
+    // 首先排除空节点的情况
+    if (!l || !r)
+      return l === r
+    // 再排除数值不相同的情况
+    if (l.val !== r.val)
+      return false
+    // 此时就是：左右节点都不为空，且数值相同的情况
+    // 递归判断外侧和内侧两种情况
+    return compare(l.left, r.right) && compare(l.right, r.left)
+  }
+  return compare(root.left, root.right)
+}
